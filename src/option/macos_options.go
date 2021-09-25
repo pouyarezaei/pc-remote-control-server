5e672d63ba9b868ad7b388e379cf686bd0aae919
pouya <pouyarezaei810@gmail.com>
2021-09-25 09:11:16 +0330

    create macos shutdown,restart,logout scaffold

---
+++ src/option/macos_options.go
@@ -0,0 +1,13 @@
+package option
+
+func ExecuteMacOsShutDownCommand() {
+	// TODO implement
+}
+
+func ExecuteMacOsLogoutCommand() {
+	// TODO implement
+}
+
+func ExecuteMacOsRestartCommand() {
+	// TODO implement
+}
